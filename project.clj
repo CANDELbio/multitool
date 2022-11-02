69c05c10f441c52ff3230a231a9ad6dadf10860c
Mike Travers <mtravers@parkerici.org>
2022-11-01 19:08:31 -0700

    update project.clj for new reality

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://github.com/ParkerICI/multitool"
   :license {:name "MIT License"
              :url "http://opensource.org/licenses/MIT"}
-  :repositories
-  [["github" {:url "https://maven.pkg.github.com/ParkerICI/mvn-packages"
-              :sign-releases false
-              :username :env/github_user
-              :password :env/github_password}]]
   :deploy-repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :source-paths ["src/cljc" "src/clj" "src/cljs"]
   :test-paths ["test/cljc" "test/clj"]  ;TODO cljs testing
+  :plugins [[lein-codox "0.10.8"]]
+  :jvm-opts ["--add-opens" "java.base/java.lang=ALL-UNNAMED"]
+  :codox {:output-path "codox"
+          :metadata {:doc/format :markdown}
+          :source-uri "http://github.com/CANDELbio/multitool/blob/master/{filepath}#L{line}"}
   )
