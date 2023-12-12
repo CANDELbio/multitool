df615be8e9ba228fccfd2d71bf358fb5133240be
Mike Travers <mt@hyperphor.com>
2023-12-11 18:28:40 -0800

    bump the right version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.candelbio/multitool "0.1.0"
+(defproject org.candelbio/multitool "0.1.1"
   :description "CANDELBio utility library"
   :url "https://github.com/CANDELbio/multitool"
   :license {:name "Apache 2 License"
              :url "https://opensource.org/licenses/Apache-2.0"}
   :deploy-repositories [["clojars" {:sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.10.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :source-paths ["src/cljc" "src/clj" "src/cljs"]
   :test-paths ["test/cljc" "test/clj"]  ;TODO cljs testing
   :plugins [[lein-codox "0.10.8"]]
