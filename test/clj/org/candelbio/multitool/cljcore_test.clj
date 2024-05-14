53d8658c16b0821bfd1efc550623b59d7b3fe61e
Mike Travers <mt@hyperphor.com>
2024-05-13 21:21:49 -0700

    increase time for pdoseq test

--- test/clj/org/candelbio/multitool/cljcore_test.clj
+++ test/clj/org/candelbio/multitool/cljcore_test.clj
@@ -30,7 +30,7 @@
     (pdoseq [i (range 100)]
             (Thread/sleep (long (rand-int 100)))
             (swap! acc conj i))
-    (Thread/sleep 200)                  
+    (Thread/sleep 400)                  
     (is (= 100 (count @acc)))           ;TODO sometimes fails
     (is (= 4950 (reduce + @acc)))
     ;; order should be scrambled (guess there is a very small chance it won't be!)
